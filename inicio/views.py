9f239c8e8d4466e03f8094583c593580550db4be
Agustin Ezeberry <aezeberry@hotmail.com>
2025-08-18 17:31:44 -0300

    agregamos valores

--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def inicio(request):
     return render(request, 'inicio.html')
 
 def crear_pelicula(request, pelicula, genero, pais):
-    return(request, 'crear_pelicula.html')
+    return render(request, 'crear_pelicula.html')
