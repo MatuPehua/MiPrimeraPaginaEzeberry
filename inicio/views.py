ebb6f8942d8acfbc3f2108e32db405c204808d4c
Agustin Ezeberry <aezeberry@hotmail.com>
2025-08-18 19:24:30 -0300

    campos 2

--- inicio/views.py
+++ inicio/views.py
@@ -7,5 +7,5 @@ def inicio(request):
 
 def crear_pelicula(request, pelicula, genero, pais):
     pelicula = Peliculas(pelicula=pelicula, genero=genero, pais=pais)
-    pelicula.save()
+    #pelicula.save()
     return render(request, 'crear_pelicula.html', {'pelicula': pelicula})
