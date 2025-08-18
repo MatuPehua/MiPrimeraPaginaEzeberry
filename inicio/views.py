fd4463bcb070fa39ee955746bda4b63a4477dd83
Agustin Ezeberry <aezeberry@hotmail.com>
2025-08-18 18:14:32 -0300

    se agrego campos

--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from inicio.models import Peliculas
 
 def inicio(request):
     return render(request, 'inicio.html')
 
 def crear_pelicula(request, pelicula, genero, pais):
-    return render(request, 'crear_pelicula.html')
+    pelicula = Peliculas(pelicula=pelicula, genero=genero, pais=pais)
+    return render(request, 'crear_pelicula.html', {'Pelicula': pelicula})
